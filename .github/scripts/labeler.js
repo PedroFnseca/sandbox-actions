f8d3a8854e9aa528ba5b7fa8ea49b8ac18c85047
pedrofnseca <pedropereira.pp264@gmail.com>
2023-03-03 18:52:31 -0300

    add try catch

--- .github/scripts/labeler.js
+++ .github/scripts/labeler.js
@@ -6,13 +6,17 @@ async function getCommits() {
   const token = process.env.GITHUB_TOKEN
   const prNumber = process.env.PR_NUMBER ?? 11
 
-  const { data: commits } = await getOctokit(token).rest.pulls.listCommits({
-    owner,
-    repo,
-    pull_number: prNumber,
-  })
-
-  return commits
+  try {
+    const { data: commits } = await getOctokit(token).rest.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: prNumber,
+    })
+  
+    return commits
+  } catch (err){
+    console.log(err)
+  }
 }
 
 async function main(){
