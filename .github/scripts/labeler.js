10b24b1257d504d676dc21bbf0f520a7e046a591
pedrofnseca <pedropereira.pp264@gmail.com>
2023-03-03 18:14:23 -0300

    :test_tube: test: enviando token

--- .github/scripts/labeler.js
+++ .github/scripts/labeler.js
@@ -1,20 +1,20 @@
 const { context, getOctokit } = require('@actions/github')
 
-const ocotkit = getOctokit(process.env.GITHUB_TOKEN)
-
 async function getCommits() {
-  const { data } = await ocotkit.repos.listCommits({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    sha: context.sha,
-    per_page: 100,
+  const { owner, repo } = context.repo
+  const token = process.env.GITHUB_TOKEN
+
+  const { data: commits } = await getOctokit(token).rest.repos.listCommits({
+    owner,
+    repo,
+    per_page: 100
   })
-  return data
+  return commits
 }
 
 async function main(){
   const commits = await getCommits()
-  const labels = commits.map(commit => commit.commit.message).join(' ').match(/(?<=\[).+?(?=\])/g)
+  const labels = commits.map(commit => commit.commit.message.match(/(?<=\[).+?(?=\])/g))
   console.log(labels)
 }
 
