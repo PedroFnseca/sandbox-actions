3100e5f62623ec9dfe5525476415db5feda97c6b
pedrofnseca <pedropereira.pp264@gmail.com>
2023-03-03 17:57:09 -0300

    :sparkles: feat: coletando commits

--- .github/scripts/labeler.js
+++ .github/scripts/labeler.js
@@ -1,1 +1,19 @@
-console.log('foi')
+const { context, getOctokit } = require('@actions/github')
+
+async function getCommits() {
+  const { owner, repo } = context.repo
+  const { data: commits } = await getOctokit().rest.repos.listCommits({
+    owner,
+    repo,
+    per_page: 100
+  })
+  return commits
+}
+
+async function main(){
+  const commits = await getCommits()
+  const labels = commits.map(commit => commit.commit.message.match(/(?<=\[).+?(?=\])/g))
+  console.log(labels)
+}
+
+main()
