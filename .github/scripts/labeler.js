6d24edf6a26417585cb53e53947767ddc94ef044
pedrofnseca <pedropereira.pp264@gmail.com>
2023-03-03 18:22:09 -0300

    :sparkles: feat: commits da própia pr

--- .github/scripts/labeler.js
+++ .github/scripts/labeler.js
@@ -2,19 +2,24 @@ const { context, getOctokit } = require('@actions/github')
 
 async function getCommits() {
   const { owner, repo } = context.repo
+  
   const token = process.env.GITHUB_TOKEN
+  const prNumber = context.payload.pull_request.number
 
-  const { data: commits } = await getOctokit(token).rest.repos.listCommits({
+  const octokit = getOctokit(token)
+
+  const { data: commits } = await octokit.pulls.listCommits({
     owner,
     repo,
-    per_page: 100
+    pull_number: prNumber
   })
+
   return commits
 }
 
 async function main(){
   const commits = await getCommits()
-  
+
   console.log(commits)
 }
 
