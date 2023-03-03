6028941cffa570ba60a6a8ba2fc176c6cf8d6963
pedrofnseca <pedropereira.pp264@gmail.com>
2023-03-03 19:03:08 -0300

    listing secrets

--- .github/scripts/labeler.js
+++ .github/scripts/labeler.js
@@ -5,20 +5,25 @@ async function getCommits() {
   const { owner, repo } = context.repo
   
   const token = process.env.GITHUB_TOKEN
-  const prNumber = process.env.PR_NUMBER ?? 11
 
-  try {
-    const { data: commits } = await getOctokit(token).rest.pulls({
-      owner: owner,
-      repo: repo,
-      pull_number: prNumber,
-      per_page: 100
-    })
+  console.log(`owner: ${owner}`)
+  console.log(`repo: ${repo}`)
+  console.log(`token: ${token}`)
+
+  // const prNumber = process.env.PR_NUMBER ?? 11
+
+  // try {
+  //   const { data: commits } = await getOctokit(token).rest.pulls({
+  //     owner: owner,
+  //     repo: repo,
+  //     pull_number: prNumber,
+  //     per_page: 100
+  //   })
   
-    return commits
-  } catch (err){
-    console.log(err)
-  }
+  //   return commits
+  // } catch (err){
+  //   console.log(err)
+  // }
 }
 
 async function main(){
